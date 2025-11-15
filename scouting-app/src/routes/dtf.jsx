19a420a4e303eb71d2af8dc45518519db9469046
raileychen <railey.ch@gmail.com>
2025-11-15 11:41:19 -0800

    updated dtf added css

--- scouting-app/src/routes/dtf.jsx
+++ scouting-app/src/routes/dtf.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import "../App.css"
 
 export default function Dtf() {
   return (
-    <div>dtf</div>
+    <div>
+      <h1> Drive Team Feeder </h1>
+        <h2> Alliance 1 </h2>
+        <form>
+          <label> Team 1 Number</label>
+            <div>
+                <input type="number" className="numberInput"/>
+            </div>
+          <label> Team 2 Number</label>
+           <div>
+                <input type="number" className="numberInput" />
+            </div>
+          <label> Team 3 Number</label>
+           <div>
+                <input type="number" className="numberInput"/>
+            </div>
+        <h2> Alliance 2 </h2>
+         <label> Team 1 Number</label>
+            <div>
+              <input type="number" className="numberInput" />
+            </div>
+          <label> Team 2 Number</label>
+           <div>
+                <input type="number" className="numberInput"/>
+          </div>
+          <label> Team 3 Number</label>
+           <div>
+                <input type="number" className="numberInput"/>
+          </div>
+    </form> 
+    <h3>
+      <button>SUBMIT</button>
+    </h3>
+      </div>
   )
 }
